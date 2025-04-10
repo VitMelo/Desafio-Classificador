49f44cde255418c4ae7dc884ec5a99bc091b6fe6
VitMelo <vmdmer@gmail.com>
2025-04-10 16:56:51 -0300

    Desafio Classificador

---
+++ DesafioNiveldeHeroi.js
@@ -0,0 +1,24 @@
+let nomeDoHeroi = "Zeus"
+let xp = 8.500 
+
+let nivel = ""
+
+if (xp < 1.000){
+    nivel = "Ferro"
+} else if(xp >= 1.001 && xp <= 2.000){
+    nivel = "Bronze"
+} else if (xp >= 2.001 && xp <= 5.000){
+    nivel = "Prata"
+} else if (xp >= 5.001 && xp <= 7.000){
+    nivel = "Ouro"
+} else if (xp >= 7.001 && xp <= 8.000){
+    nivel = "Platina diamante"
+} else if (xp >= 8.001 && xp <= 9.000){
+    nivel = "Ascendente"
+} else if (xp >= 9.001 && xp <= 10.000){
+    nivel = "Imortal"
+} else if (xp >= 10.001){
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome, " + nomeDoHeroi + " está no nível de: " + nivel)
